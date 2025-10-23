bb7fb51fdb1c9611fe20501c71e86acb9e335f2a
Kristoffer Hemm <kristoffer.hemm@gmail.com>
2025-10-23 16:39:51 +0200

    made page five in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import pandas as pd
+from pymongo import MongoClient
 from second import second_page
 from third import third_page
 from fourth import fourth_page
+from fifth import fifth_page
+
+# Connecting to MongoDB
+uri = st.secrets["mongo"]["uri"]
+db_name = st.secrets["mongo"]["database"]
+client = MongoClient(uri)
+db = client[db_name]
+
+# Read data from MongoDB
+users_collection = db["production_NO1"]
+st.write(list(users_collection.find()))
 
 # Define pages as functions
 def home():
@@ -21,8 +33,12 @@ def fourth():
     st.title("Fourth Page")
     fourth_page()
 
+def fifth():
+    st.title("Fifth Page")
+    fifth_page()
+
 # Sidebar navigation
-page = st.sidebar.selectbox("Navigate", ["Home", "Second", "Third", "Fourth"])
+page = st.sidebar.selectbox("Navigate", ["Home", "Second", "Third", "Fourth", "Fifth"])
 
 # Display the selected page
 if page == "Home":
@@ -33,3 +49,5 @@ elif page == "Third":
     third()
 elif page == "Fourth":
     fourth()
+elif page == "Fifth":
+    fifth()
