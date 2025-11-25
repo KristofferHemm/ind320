a345aa8b20f2571cf0638818251b2bf7b7f84463
Kristoffer Hemm <kristoffer.hemm@gmail.com>
2025-11-25 13:30:05 +0100

    updated plots from part 3 containing errors

--- streamlit-app/newB.py
+++ streamlit-app/newB.py
@@ -21,20 +21,24 @@ def plot_summary_satv(df, cutoff=100, k=3.0):
     """
 
     df = df.copy()
-    
+
     # High pass filtering
-    temp_dct_ortho = dct(df.temperature_2m, norm='ortho')
+    temp_dct_ortho = dct(df['temperature_2m'], norm='ortho')
     temp_dct_ortho[:cutoff] = 0
     df['temp_highpass'] = idct(temp_dct_ortho, norm='ortho')
+    df['seasonal'] = df['temperature_2m'] - df['temp_highpass']
 
     # Seasonally adjusted temperature variations
     median_satv = np.median(df['temp_highpass'])
     mad_satv = np.median(np.abs(df['temp_highpass'] - median_satv))
-    ucl = median_satv + k * mad_satv
-    lcl = median_satv - k * mad_satv
+    upper_satv = median_satv + k * mad_satv
+    lower_satv = median_satv - k * mad_satv
+
+    df['ucl'] = df['seasonal'] + upper_satv
+    df['lcl'] = df['seasonal'] + lower_satv
 
     # Add outliers to df
-    df['is_outlier'] = (df['temp_highpass'] > ucl) | (df['temp_highpass'] < lcl)
+    df['is_outlier'] = (df['temp_highpass'] > upper_satv) | (df['temp_highpass'] < lower_satv)
 
     # Plotting
     fig = go.Figure()
@@ -58,16 +62,16 @@ def plot_summary_satv(df, cutoff=100, k=3.0):
 
     # SPC boundaries (horizontal lines)
     fig.add_trace(go.Scatter(
-        x=[df['date'].min(), df['date'].max()],
-        y=[ucl, ucl],
+        x=df['date'],
+        y=df['ucl'],
         mode='lines',
         name='UCL (Upper Control Limit)',
         line=dict(color='green', dash='dash')
     ))
     
     fig.add_trace(go.Scatter(
-        x=[df['date'].min(), df['date'].max()],
-        y=[lcl, lcl],
+        x=df['date'],
+        y=df['lcl'],
         mode='lines',
         name='LCL (Lower Control Limit)',
         line=dict(color='green', dash='dash')
@@ -109,7 +113,17 @@ def plot_summary_satv(df, cutoff=100, k=3.0):
 
     return fig, summary
 
-def plot_precip_anomalies(df, outlier_fraction=0.01):
+def plot_summary_lof(df, outlier_fraction=0.01):
+    """
+    Input:
+    df : pd.DataFrame
+    outlier_fraction : float
+
+    Returns
+    -------
+    fig : plotly.graph_objects.Figure
+    summary : pd.DataFrame
+    """
     
     df = df.copy()
     df["date"] = pd.to_datetime(df["date"])
@@ -121,16 +135,14 @@ def plot_precip_anomalies(df, outlier_fraction=0.01):
     lof = LocalOutlierFactor(n_neighbors=20, contamination=outlier_fraction)
     labels = lof.fit_predict(X)
     df["anomaly"] = labels == -1
-    
     anomalies = df[df["anomaly"]]
-    normal = df[~df["anomaly"]]
 
-    # Create Plotly figure 
+    # Plotting
     fig = go.Figure()
 
     # Normal points (blue line)
     fig.add_trace(go.Scatter(
-        x=normal["date"], y=normal["precipitation"],
+        x=df["date"], y=df["precipitation"],
         mode="lines", name="Normal", line=dict(color="blue")
     ))
 
@@ -149,11 +161,12 @@ def plot_precip_anomalies(df, outlier_fraction=0.01):
         legend_title="Type"
     )
 
+    # Create summary
     summary_data = {
             "Total Points": [len(df)],
             "Detected Anomalies": [len(anomalies)],
             "Anomaly %": [len(anomalies) / len(df) * 100],
-            "Normal Mean": [normal["precipitation"].mean()],
+            "Normal Mean": [df["precipitation"].mean()],
             "Anomaly Mean": [anomalies["precipitation"].mean()],
             "Anomaly Max": [anomalies["precipitation"].max()],
             "Anomaly Min": [anomalies["precipitation"].min()],
@@ -163,43 +176,3 @@ def plot_precip_anomalies(df, outlier_fraction=0.01):
     summary = pd.DataFrame(summary_data)
 
     return fig, summary
-
-
-
-def newB_page():
-    
-    st.write("Please choose which plot you want to see below.")
-
-    tab1, tab2 = st.tabs(["SPC analysis", "LOF analysis"])
-
-    if 'selected_year' in st.session_state:
-        selected_year = st.session_state.selected_year
-        
-    else:
-        st.warning("Please select a year on the second page first")
-
-    if 'selected_city' in st.session_state:
-        selected_city = st.session_state.selected_city
-    else:
-        st.warning("Please select a city on the second page first")
-
-    # Load data
-    df = load_data_from_meteo(st.session_state.selected_year, st.session_state.selected_city)
-    
-    # Content for Tab 1
-    with tab1:
-        st.header("SPC analysis")
-        st.write(f"Using data from year: {selected_year}")
-        st.write(f"Using data from city: {selected_city}")
-        fig, summary = plot_summary_satv(df)
-        st.plotly_chart(fig)
-        st.write(summary.head())
-
-    # Content for Tab 2
-    with tab2:
-        st.header("LOF analysis")
-        st.write(f"Using data from year: {selected_year}")
-        st.write(f"Using data from city: {selected_city}")
-        fig, summary = plot_precip_anomalies(df)
-        st.plotly_chart(fig)
-        st.write(summary)
